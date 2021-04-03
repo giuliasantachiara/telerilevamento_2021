3eb0bec110da05abc5ee42779f5c83cac992c8f0
giuliasantachiara <81862267+giuliasantachiara@users.noreply.github.com>
2021-04-03 17:28:33 +0200

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -6,7 +6,17 @@ setwd("/Users/Giulia/desktop/lab/")
 #install.packages("raster")
 library("raster")
 
+#il comando brick mi serve per importare dentro ad R
 p224r63_2011<-brick("p224r63_2011_masked.grd")
-
-#per avere visualizzazione dei dati
+# comando per avere visualizzazione dei dati
 plot(p224r63_2011)
+#noi in questo caso abbiamo 7 bande
+
+#comando per cambiare colore dei plot e quindi delle bande, 
+# con il primo comando creo una nuova palette di colori, mentre con il secondo comando inserico la palette
+
+cl<-colorRampPalette(c("black","grey","light grey"))(100)
+plot(p224r63_2011,col=cl)
+#prova colori 
+clnosens<-colorRampPalette(c("red","green","blue"))(100)
+plot(p224r63_2011,col=clnosens)

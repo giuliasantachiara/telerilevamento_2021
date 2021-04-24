9950670b5fec9612add1a6eb70193c11719d6b1d
giuliasantachiara <81862267+giuliasantachiara@users.noreply.github.com>
2021-04-24 16:45:39 +0200

    Create R_code_time_series.r

---
+++ R_code_time_series.r
@@ -0,0 +1,7 @@
+#time series analysis
+#greenland increase of temperature
+#data and code from Emanuela Costa
+
+library(raster)
+#prendiamo la cartella giusta
+setwd("/Users/Giulia/desktop/lab/greenland")

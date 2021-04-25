44cb3ed61ea68ff972eef90dae1a113d6bd491da
giuliasantachiara <81862267+giuliasantachiara@users.noreply.github.com>
2021-04-25 12:12:58 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -2,6 +2,17 @@
 #greenland increase of temperature
 #data and code from Emanuela Costa
 
+#carichiamo il pacchetto raster
 library(raster)
 #prendiamo la cartella giusta
 setwd("/Users/Giulia/desktop/lab/greenland")
+#dati che analizziamo sono presi da copernicus e sono delle temperature
+
+#carichiamo i singoli dati
+lst_2000<-raster("lst_2000.tif")
+#e carichiamo l'immagine
+plot(lst_2000)
+lst_2005<-raster("lst_2005.tif")
+plot(lst_2005)
+
+

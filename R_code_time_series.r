0b7c3f3f8c73dcb5a930e0172e50335992fd132c
giuliasantachiara <81862267+giuliasantachiara@users.noreply.github.com>
2021-04-25 12:55:19 +0200

    Update R_code_time_series.r

--- R_code_time_series.r
+++ R_code_time_series.r
@@ -14,5 +14,30 @@ lst_2000<-raster("lst_2000.tif")
 plot(lst_2000)
 lst_2005<-raster("lst_2005.tif")
 plot(lst_2005)
+lst_2010<-raster("lst_2010.tif")
+plot(lst_2010)
+lst_2015<-raster("lst_2015.tif")
+plot(lst_2015)
+#mettiamo nella stessa schermata tutte le immagini per avere visione generale
+par(mfrow=c(2,2))
+plot(lst_2000)
+plot(lst_2005)
+plot(lst_2010)
+plot(lst_2015)
+dev.off()
 
-
+#lapply funzione che mi permette di applicare la funzione raster a una lista di file. con un solo comando carico tante informazionià
+#prima quindi facciamo la lista dei file che voglio caricare su R, nel pattern mettiamo il nume che tutti i file hanno in comune
+rlist<-list.files(pattern="tif")
+rlist
+#una volta fatta la lista faccio il comando che applica a tutti la stessa funzione
+import<-lapply(rlist, raster)
+import
+#impacchettiamo tutti i file insieme con la funzione stack e li plottiamo, quindi con una funzione facciamo quello che facevano con tanti comandi
+TGr<-(stack(import))
+plot(TGr)
+#proviamo a plottare immagine con classificazione RGB
+plotRGB(TGr,1,2,3,stretch="Lin")
+#se abbiamo più colori scuri allora vuol dire che impatta di più l'immagine che si trova nella banda blu
+#proviamo a invertire le immagini
+plotRGB(TGr,2,3,4,stretch="Lin")

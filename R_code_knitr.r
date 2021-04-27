6945ce793fb7b57971b66bf117b9a4383e89ea7d
giuliasantachiara <81862267+giuliasantachiara@users.noreply.github.com>
2021-04-27 12:00:41 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -3,9 +3,10 @@
 
 
 #indirizziamo nella giusta working directory
-setwd("/Users/Giulia/desktop/lab/greenland/")
+setwd("/Users/Giulia/desktop/")
 
 library(knitr)
 
 #andiamo a trovare il nostro codice che abbiamo creato in precedenza
-stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr"))
+stitch("R_code_greenland.txt", template=system.file("misc", "knitr-template.Rnw", package="knitr"))à
+#con questo creiamo un file .tex, che poi andiamo a modificare e da cui andiamo a generare un pdf
